ffd6b68b926f48d886798d72b84981b13271e98f
francocn1995@gmail.com <francocn1995@gmail.com>
2022-05-18 14:22:09 -0300

    se agrego la logica para crear producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,15 +1,38 @@
+import Producto from "../models/producto";
+
 const productCtrl = {};
 
 //agregamos la logica para obtener la lista de productos
 
-productCtrl.listarProductos = (req, res)=>{
-    //toda la logica que quiero que suceda para obtener la lista
-    res.send ('hola desde el backend')
-}
+productCtrl.listarProductos = (req, res) => {
+  //toda la logica que quiero que suceda para obtener la lista
+  res.send("hola desde el backend");
+};
 
-productCtrl.crearProducto = (req, res)=>{
-    console.log(req.body)
-    res.send ('cree un producto')
-}
+productCtrl.crearProducto = async (req, res) => {
+  try {
+    console.log(req.body);
+    //validar
+    //crear producto en la bd
+    const productoNuevo = new Producto({
+      productName: req.body.productName,
+      price: req.body.price,
+      urlImg: req.body.urlImg,
+      category: req.body.category,
+    });
+    // guardar en la base de datos
+    await productoNuevo.save();
+    //enviar respuesta
+    res.status(201).json({
+      mensaje: "Product create",
+    });
+  } catch (error) {
+    console.log(error);
+    //enviar codigo de error
+    res.status(404).json({
+      mensaje: "error al intentar agregar un producto",
+    });
+  }
+};
 
 export default productCtrl;
--- src/models/producto.js
+++ src/models/producto.js
@@ -1,1 +1,30 @@
 // modelamos el dato a guardar en la db
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema({
+    productName: {
+        type: String,
+        maxlength:200,
+        unique: true,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxlength: 2000,
+    },
+    urlImg :{
+        type: String,
+        required: true,
+
+    },
+    category: {
+        type: String,
+        required: true,
+        
+    }
+});
+
+const Producto = mongoose.model('producto', productoSchema)
+
+export default Producto;
